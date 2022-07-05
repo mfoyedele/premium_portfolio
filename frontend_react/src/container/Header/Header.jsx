54e8767d0228a907caa287c00dcccfa10444bc4b
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2022-07-05 17:45:40 +0100

    Update Header.jsx

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -33,8 +33,8 @@ const Header = () => (
         </div>
 
         <div className='tag-cmp app__flex'>
-          <p className='p-text'>Web Developer</p>
-          <p className='p-text'>Freelancer</p>
+          <p className='p-text'>Software Engineer</p>
+          <p className='p-text'>IT Enthusiast</p>
         </div>
       </div>
     </motion.div>
@@ -59,7 +59,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className='app__header-circles'
     >
-      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+      {[images.dsa, images.node, images.cpp].map((circle, index) => (
         <div className='circle-cmp app__flex' key={`circle-${index}`}>
           <img src={circle} alt='profile_bg' />
         </div>
